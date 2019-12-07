bd3d24d99524a4f3df0e7a907197fdaa419f7ea7
Sergey Gomongulov <support@omobus.net>
2019-12-07 18:18:57 +0300

    3.4.2 release (updated build scripts).

--- build-release.sh
+++ build-release.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 
-./gradlew clean && ./gradlew assembleRelease && cp ./build/outputs/apk/release/omobus-applog-demo-*.apk ../ && ./gradlew clean
+vstamp='none'
+
+for i in `grep propVersionName ./gradle.properties | tr '=' '\n'`
+do
+    vstamp=$i
+done;
+
+./gradlew clean
+./gradlew assembleRelease
+cp ./build/outputs/apk/release/omobus-applog-demo-release.apk ../omobus-applog-demo-$vstamp.apk
+./gradlew clean
+
+echo "omobus-applog-demo $vstamp has been compiled."
